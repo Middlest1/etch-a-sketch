6ae2383c9b584ec3da058083770279928e89095e
Ben Barr <benjamincorybarr@gmail.com>
2024-03-08 11:42:49 -0800

    Adapt grid area to user's input

--- etch.js
+++ etch.js
@@ -10,15 +10,30 @@
 // Implement a progressive darkening effect where each interaction adds 10% more black or color to the square. The objective is to achieve a completely black square only after ten interactions.
 
 const selectOutputDiv = document.querySelector('#gridContainer');
+const containerWidth = 960;
+
+
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+console.log(Math.random())
+console.log(randomRGB());
 
 
 function createGrid(numOfSquares) {
 
-    while (numOfSquares > 0) {
+    let squareSize = containerWidth / numOfSquares;
+
+    for (let i = 0; i < numOfSquares ** 2; i++) {
         let newSquare = document.createElement("div");
         newSquare.classList.add('grid');
+        newSquare.style.width = `${squareSize}px`;
+        newSquare.style.height = `${squareSize}px`;
         selectOutputDiv.appendChild(newSquare);
-        numOfSquares--;
     }
 
     let squareOfGrid = document.querySelectorAll('.grid');
@@ -31,7 +46,7 @@ function createGrid(numOfSquares) {
     })
 }
 
-createGrid(256);
+createGrid(16);
 
 
 
@@ -43,7 +58,7 @@ changeGridButton.addEventListener('click', function () {
     let userEnteredNumOfSquares = 0;
 
     while (userEnteredNumOfSquares < 1 || userEnteredNumOfSquares > 100) {
-        let userInput = prompt('How many squares per side would you like? Please enter a number less than 100 and greater than 0.')
+        let userInput = prompt('How many squares per row would you like? Please enter a number less than 100 and greater than 0. \n (A bigger number will allow you to do more detailed sketches.)')
         userEnteredNumOfSquares = userInput;
 
         if (userInput == null) {
