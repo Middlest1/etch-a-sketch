9fbaa1e46a8eafb768cadcf637314afc936a066f
Ben Barr <benjamincorybarr@gmail.com>
2024-03-07 16:09:58 -0800

    Create 16 divs with same class name

--- etch.js
+++ etch.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+
+
+function createSquare(numOfSquares) {
+
+    while (numOfSquares > 0) {
+
+        const selectOutputDiv = document.querySelector('#gridContainer');
+        let newSquare = document.createElement("div");
+        newSquare.classList.add('grid');
+        selectOutputDiv.appendChild(newSquare);
+        numOfSquares--;
+
+    }
+}
+
+createSquare(16);
